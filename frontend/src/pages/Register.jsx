d6eb2d6510517694201e6f73927b92fd0a9f87d4
dakshina5206 <dakshina.alahakoon@gmail.com>
2025-06-28 18:57:16 +1000

    added input fields to registration

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -7,5 +7,36 @@ export default function Register() {
     email: '',
     password: '',
   });
-  return <div>TODO: Implement Register Page</div>;
+  return (
+    <div>
+      <h1>Register</h1>
+      <form>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={form.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Username"
+          value={form.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 } 
